0086ba12eef5b8cf6f92723e540c24c9fe91b29f
Jior <jior.mail@gmail.com>
2013-06-26 20:11:58 +0400

    change to jquery

--- source/app.d
+++ source/app.d
@@ -4,13 +4,23 @@ void index(HTTPServerRequest req, HTTPServerResponse res) {
 	res.renderCompat!("index.dt", HTTPServerRequest, "req")(req);
 }
 
+void handleRequest(WebSocket socket) {
+	while( socket.connected ) {
+		if( socket.dataAvailableForRead() ) {
+			auto data = socket.receive();
+			socket.send(cast(string) data);
+		}
+	}
+}
+
 shared static this() {
 	auto router = new URLRouter;
 	router.get("/", &index);
+	router.get("/ws", handleWebSockets(&handleRequest));
 	router.get("/static/*", serveStaticFiles("./static/", new HTTPFileServerSettings("/static/")));
 
 	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+	settings.port = 8000;
 
 	listenHTTP(settings, router);
 }
